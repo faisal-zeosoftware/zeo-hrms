10a968951b0fa6062eeea81e5fa36fdf1ccfbafc
subina <subina@gmail.com>
2025-07-19 17:18:30 +0530

    add

--- OrganisationManager/apps.py
+++ OrganisationManager/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OrganisationmanagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'OrganisationManager'
+
+    def ready(self):
+        import OrganisationManager.signals

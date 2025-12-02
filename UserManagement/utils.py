35ade884b506d50fa56fb5d3d3addffbf8fcf044
subina <subinasubi2000@gmail.com>
2025-12-02 16:33:13 +0530

    otp

--- UserManagement/utils.py
+++ UserManagement/utils.py
@@ -8,3 +8,15 @@ def get_tokens_for_user(user, tenant_schema):
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
+
+import random
+from django.utils import timezone
+from datetime import timedelta
+
+def generate_otp():
+    return str(random.randint(100000, 999999))
+
+def is_otp_valid(user):
+    if not user.otp_created_at:
+        return False
+    return timezone.now() <= user.otp_created_at + timedelta(minutes=5)

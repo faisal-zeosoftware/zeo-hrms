5ee191490c139ac79653a5fbbf5f8b6949060563
subina <subinasubi2000@gmail.com>
2026-01-06 12:01:07 +0530

    adds

--- calendars/models.py
+++ calendars/models.py
@@ -1981,7 +1981,7 @@ class EmployeeOvertime(models.Model):
 
     ot_type = models.CharField(
         max_length=10,
-        choices=OT_TYPE_CHOICES
+        choices=OT_TYPE_CHOICES,null=True, blank=True
     )
 
     hours = models.DecimalField(max_digits=6, decimal_places=2)
@@ -2017,7 +2017,7 @@ class OvertimePolicy(models.Model):
         ('HOLIDAY', 'Holiday OT'),
     )
     name = models.CharField(max_length=100)
-    ot_type = models.CharField(max_length=10,choices=OT_TYPE_CHOICES)
+    ot_type = models.CharField(max_length=10,choices=OT_TYPE_CHOICES,null=True, blank=True)
     rate_multiplier = models.DecimalField( max_digits=4, decimal_places=2,help_text="Example: 1.5, 2.0")
     # Applicability (ALL OPTIONAL)
     branch = models.ForeignKey( 'OrganisationManager.brnch_mstr', null=True, blank=True, on_delete=models.CASCADE )

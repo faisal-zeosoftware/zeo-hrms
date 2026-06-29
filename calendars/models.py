f14d8f1a071daee740e5ccc0cbbaa6386ec83478
subina <subinasunil@gmail.com>
2026-06-29 13:24:37 +0530

    add

--- calendars/models.py
+++ calendars/models.py
@@ -1970,7 +1970,7 @@ class AttendancePolicy(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} - {self.branch.name}"    
+        return f"{self.name} - {self.branch.branch_name}"    
 
 class AttendanceValidationPolicy(models.Model):
     name = models.CharField(max_length=100)
